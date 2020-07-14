802fd99e71dd0d2da0d983b8f5692dec535bd8eb
danghuutoan <danghuutoan2002@gmail.com>
2020-07-14 11:12:29 +0700

    implementing next button

--- plugins.js
+++ plugins.js
@@ -1,17 +1,35 @@
-element = document.getElementById("img-1");
-element.onclick = () => {
-	console.log("click", element);
+imgHandleClick = (e) => {
+	const album = document.querySelectorAll(".lightbox-img");
+	const albumLength = album.length;
+	let currentIndex = 0;
+	const element = e.target;
+	console.log(typeof element);
+	for (let i = 0; i < album.length; i++) {
+		if (element === album[i]) {
+			currentIndex = i;
+		}
+	}
+
 	const lightboxId = element.dataset.target;
+	const lightboxImg = document.getElementById("lightbox-img");
+	lightboxImg.src = e.target.attributes.src.value;
 	const lightbox = document.getElementById(lightboxId);
 	lightbox.classList.remove("lightbox-hide");
-	console.log(lightbox);
-};
 
-exitBtn = document.getElementById("lightbox-exit-btn");
+	exitBtn = document.getElementById("lightbox-exit-btn");
 
-exitBtn.onclick = () => {
-	const lightboxId = element.dataset.target;
-	const lightbox = document.getElementById(lightboxId);
-	lightbox.classList.add("lightbox-hide");
-	console.log(lightbox);
+	exitBtn.onclick = (e) => {
+		lightbox.classList.add("lightbox-hide");
+	};
+
+	nextBtn = document.getElementById("lightbox-next-btn");
+
+	nextBtn.onclick = (e) => {
+		if (currentIndex + 1 > albumLength - 1) {
+			return;
+		} else {
+			currentIndex = currentIndex + 1;
+			lightboxImg.src = album[currentIndex].attributes.src.value;
+		}
+	};
 };
