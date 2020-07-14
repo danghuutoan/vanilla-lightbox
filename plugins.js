e9c116cfa02c92a6b732edcb8fe29796281ca57a
danghuutoan <danghuutoan2002@gmail.com>
2020-07-14 11:45:10 +0700

    refactoring code

--- plugins.js
+++ plugins.js
@@ -1,46 +1,55 @@
-imgHandleClick = (e) => {
-	const album = document.querySelectorAll(".lightbox-img");
-	const albumLength = album.length;
-	let currentIndex = 0;
-	const element = e.target;
+const lightbox = document.getElementById("lightbox-bg");
+const album = document.querySelectorAll(".lightbox-img");
+const lightboxImg = document.getElementById("lightbox-img");
+const albumLength = album.length;
+let currentIndex = 0;
 
+exitBtn = document.getElementById("lightbox-exit-btn");
+exitBtn.onclick = (e) => {
+	lightbox.classList.add("lightbox-hide");
+};
+
+nextBtn = document.getElementById("lightbox-next-btn");
+nextBtn.onclick = (e) => {
+	if (currentIndex + 1 > albumLength - 1) {
+		return;
+	} else {
+		currentIndex = currentIndex + 1;
+		lightboxImg.src = album[currentIndex].attributes.src.value;
+	}
+};
+
+prevBtn = document.getElementById("lightbox-prev-btn");
+prevBtn.onclick = (e) => {
+	if (currentIndex - 1 < 0) {
+		currentIndex = 0;
+	} else {
+		currentIndex = currentIndex - 1;
+		lightboxImg.src = album[currentIndex].attributes.src.value;
+	}
+};
+
+displayLightbox = () => {
+	lightbox.classList.remove("lightbox-hide");
+};
+
+setLightboxImage = (value) => {
+	lightboxImg.src = value;
+};
+
+getCurrentImageIndex = (album, imageElement) => {
+	let currentIndex = null;
 	for (let i = 0; i < album.length; i++) {
-		if (element === album[i]) {
+		if (imageElement === album[i]) {
 			currentIndex = i;
 		}
 	}
+	return currentIndex;
+};
 
-	const lightboxId = element.dataset.target;
-	const lightboxImg = document.getElementById("lightbox-img");
-	lightboxImg.src = e.target.attributes.src.value;
-	const lightbox = document.getElementById(lightboxId);
-	lightbox.classList.remove("lightbox-hide");
-
-	exitBtn = document.getElementById("lightbox-exit-btn");
-
-	exitBtn.onclick = (e) => {
-		lightbox.classList.add("lightbox-hide");
-	};
-
-	nextBtn = document.getElementById("lightbox-next-btn");
-
-	nextBtn.onclick = (e) => {
-		if (currentIndex + 1 > albumLength - 1) {
-			return;
-		} else {
-			currentIndex = currentIndex + 1;
-			lightboxImg.src = album[currentIndex].attributes.src.value;
-		}
-	};
-
-	prevBtn = document.getElementById("lightbox-prev-btn");
-
-	prevBtn.onclick = (e) => {
-		if (currentIndex - 1 < 0) {
-			currentIndex = 0;
-		} else {
-			currentIndex = currentIndex - 1;
-			lightboxImg.src = album[currentIndex].attributes.src.value;
-		}
-	};
+imgHandleClick = (e) => {
+	const element = e.target;
+	currentIndex = getCurrentImageIndex(album, element);
+	setLightboxImage(element.attributes.src.value);
+	displayLightbox();
 };
